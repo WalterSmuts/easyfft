d6f0231b94e66f80a885716b5d602bfe380ef7c5
Walter Smuts <smuts.walter@gmail.com>
2022-10-20 13:48:22 +0200

    Add static scratch buffer to const size fft

--- src/const_size.rs
+++ src/const_size.rs
@@ -22,6 +22,8 @@
 //!     assert_ulps_eq!(manipulated.im, original.im * complex_signal.len() as f64);
 //! }
 //! ```
+use std::cell::UnsafeCell;
+
 #[rustfmt::skip]
 use ::realfft::num_complex::Complex;
 #[rustfmt::skip]
@@ -61,6 +63,39 @@ pub trait IfftMut<T, const SIZE: usize> {
     fn ifft_mut(&mut self);
 }
 
+trait StaticScratchFft<T: FftNum, const SIZE: usize>: rustfft::Fft<T> {
+    fn process_with_static_scratch(&self, buffer: &mut [Complex<T>; SIZE]);
+}
+
+impl<T: FftNum + Default, U: ?Sized + rustfft::Fft<T>, const SIZE: usize> StaticScratchFft<T, SIZE>
+    for U
+{
+    fn process_with_static_scratch(&self, buffer: &mut [Complex<T>; SIZE]) {
+        let scratch_buffer_pointer =
+            generic_singleton::get_or_init(|| UnsafeCell::new([Complex::default(); SIZE])).get();
+        // SAFETY:
+        // Issue:
+        // * The pointer must be properly aligned.
+        // * It must be "dereferenceable" in the sense defined in [the module documentation].
+        // * The pointer must point to an initialized instance of `T`.
+        // Proof:
+        // These invariants are all guaranteed by the generic_singleton crate.
+        //
+        // Issue:
+        // * You must enforce Rust's aliasing rules, since the returned lifetime `'a` is
+        //   arbitrarily chosen and does not necessarily reflect the actual lifetime of the data.
+        //   In particular, while this reference exists, the memory the pointer points to must
+        //   not get accessed (read or written) through any other pointer.
+        //
+        // Proof:
+        // The pointer points towards thread-local storage and is only converted to a reference in
+        // this function, without leaking references. Therefore the exclusive reference invariant
+        // should hold.
+        let scratch_buffer_ref = unsafe { scratch_buffer_pointer.as_mut().unwrap_unchecked() };
+        self.process_with_scratch(buffer, scratch_buffer_ref);
+    }
+}
+
 impl<T: FftNum + Default, const SIZE: usize> Fft<T, SIZE> for [T; SIZE] {
     fn fft(&self) -> [Complex<T>; SIZE] {
         // SAFETY:
@@ -79,16 +114,16 @@ impl<T: FftNum + Default, const SIZE: usize> Fft<T, SIZE> for [T; SIZE] {
             .unwrap_unchecked()
         };
 
-        get_fft_algorithm::<T>(SIZE).process(&mut buffer);
+        get_fft_algorithm::<T>(SIZE).process_with_static_scratch(&mut buffer);
         buffer
     }
 }
 
-impl<T: FftNum, const SIZE: usize> Fft<T, SIZE> for [Complex<T>; SIZE] {
+impl<T: FftNum + std::default::Default, const SIZE: usize> Fft<T, SIZE> for [Complex<T>; SIZE] {
     fn fft(&self) -> [Complex<T>; SIZE] {
         // Copy into a new buffer
         let mut buffer: [Complex<T>; SIZE] = *self;
-        get_fft_algorithm::<T>(SIZE).process(&mut buffer);
+        get_fft_algorithm::<T>(SIZE).process_with_static_scratch(&mut buffer);
         buffer
     }
 }
@@ -96,20 +131,20 @@ impl<T: FftNum, const SIZE: usize> Fft<T, SIZE> for [Complex<T>; SIZE] {
 impl<T: FftNum + Default, const SIZE: usize> Ifft<T, SIZE> for [Complex<T>; SIZE] {
     fn ifft(&self) -> [Complex<T>; SIZE] {
         let mut buffer = *self;
-        get_inverse_fft_algorithm::<T>(SIZE).process(&mut buffer);
+        get_inverse_fft_algorithm::<T>(SIZE).process_with_static_scratch(&mut buffer);
         buffer
     }
 }
 
-impl<T: FftNum, const SIZE: usize> FftMut<T, SIZE> for [Complex<T>; SIZE] {
+impl<T: FftNum + std::default::Default, const SIZE: usize> FftMut<T, SIZE> for [Complex<T>; SIZE] {
     fn fft_mut(&mut self) {
-        get_fft_algorithm::<T>(SIZE).process(self);
+        get_fft_algorithm::<T>(SIZE).process_with_static_scratch(self);
     }
 }
 
 impl<T: FftNum + Default, const SIZE: usize> IfftMut<T, SIZE> for [Complex<T>; SIZE] {
     fn ifft_mut(&mut self) {
-        get_inverse_fft_algorithm::<T>(SIZE).process(self);
+        get_inverse_fft_algorithm::<T>(SIZE).process_with_static_scratch(self);
     }
 }
 
